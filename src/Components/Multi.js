ad8a0d728eaa0ec7b062cdecb109814519cbb1fe
Esha Patel <Patellesha11@gmail.com>
2025-04-18 22:16:46 +0300

    added formdata

--- src/Components/Multi.js
+++ src/Components/Multi.js
@@ -1,7 +1,18 @@
 import { useState } from "react";
 
 function Form () {
+        const [formData, setFormData] = useState({
+            name: "",
+            email: "",
+            phone: "",
+            address: "",
+            city: ""
+          });
+
     return (
-        
-    )
-}
+        <div>
+
+        </div>
+         
+    );
+};
