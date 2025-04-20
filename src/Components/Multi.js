df1e75090ae46340b0b010bebacbe6cb346074ec
Esha Patel <Patellesha11@gmail.com>
2025-04-20 16:49:46 +0300

    imported css

--- src/Components/Multi.js
+++ src/Components/Multi.js
@@ -4,7 +4,7 @@ import Step2 from './Step2';
 import Step3 from './Step3';
 import Step4 from './Step4';
 import Step5 from './Step5';
-import './Form.css';
+import "../Styles/Form.css";
 
 function Form () {
         const [step, setStep] = useState(0);
