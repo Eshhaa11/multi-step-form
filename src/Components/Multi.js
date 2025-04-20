2b2e46697111f8ffb3cb50096d85edd5f85a7cfc
Esha Patel <Patellesha11@gmail.com>
2025-04-20 16:38:41 +0300

    imported form.css

--- src/Components/Multi.js
+++ src/Components/Multi.js
@@ -4,6 +4,7 @@ import Step2 from './Step2';
 import Step3 from './Step3';
 import Step4 from './Step4';
 import Step5 from './Step5';
+import './Form.css';
 
 function Form () {
         const [formData, setFormData] = useState({
